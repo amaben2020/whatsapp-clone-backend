54cc18ac3c43770c06f231aa75d74fb2260a1416
algomachine007 <amaben202phantom@gmail.com>
2024-02-18 17:09:24 +0100

    handling errors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,35 @@ import { logger } from "./configs/logger.js";
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+let server;
+
+server = app.listen(PORT, () => {
   logger.info("Running...");
+});
+
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+// the error here is passed as an event
+const unhandledExceptionErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+// handling error gracefully
+process.on("uncaughtException", unhandledExceptionErrorHandler);
+process.on("unhandledRejection", unhandledExceptionErrorHandler);
+
+// signal termination
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed");
+    process.exit(1);
+  }
 });
