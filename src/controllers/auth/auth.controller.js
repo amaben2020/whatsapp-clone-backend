47db72a9f6850275acbda9c090abdfc7d6778828
algomachine007 <amaben202phantom@gmail.com>
2024-02-22 21:41:18 +0100

    login func

--- src/controllers/auth/auth.controller.js
+++ src/controllers/auth/auth.controller.js
@@ -1,6 +1,10 @@
+import bcrypt from "bcrypt";
+import asyncHandler from "express-async-handler";
+import pkg from "http-errors";
+import UserModel from "../../models/UserModel.js";
 import { createUser } from "../../utils/createUser.js";
 import { generateToken } from "../../utils/generateToken.js";
-
+const { createHttpError } = pkg;
 export const register = async (req, res, next) => {
   try {
     const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process?.env;
@@ -39,7 +43,6 @@ export const register = async (req, res, next) => {
     });
 
     // send the user with access_token and data
-
     res.status(201).json({
       message: "Registration successful",
       access_token,
@@ -55,13 +58,57 @@ export const register = async (req, res, next) => {
     next(error);
   }
 };
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-export const logout = (req, res) => {
+  const user = await UserModel.findOne({ email }).lean();
+
+  // check if password matched
+  const isUserPassword = await bcrypt.compare(password, user?.password);
+
+  if (!isUserPassword || !user) {
+    throw createHttpError.BadRequest("User does not exist");
+  }
+
+  const access_token = await generateToken(
+    {
+      userId: user?._id,
+    },
+    ACCESS_TOKEN_SECRET,
+    "1d",
+  );
+
+  const refresh_token = await generateToken(
+    {
+      userId: user?._id,
+    },
+    REFRESH_TOKEN_SECRET,
+    "30d",
+  );
+
+  // attach the refresh token to cookie
+  res.cookie("refreshToken", refresh_token, {
+    httpOnly: true,
+    path: "/api/v1/auth/refreshToken",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+
+  res.status(200).json({
+    message: "User login successful",
+    access_token: access_token,
+    user: {
+      name: user?.name,
+      email: user?.email,
+      picture: user?.picture,
+      status: user?.status,
+    },
+  });
+});
+
+export const logout = asyncHandler(async (req, res) => {
   res.send("Oks");
-};
-export const login = (req, res) => {
+});
+
+export const refreshToken = asyncHandler(async (req, res) => {
   res.send("Oks");
-};
-export const refreshToken = (req, res) => {
-  res.send("Oks");
-};
+});
