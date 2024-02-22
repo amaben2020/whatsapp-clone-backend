6a316a8e99e972e662de92c3de9cd49085204dbb
algomachine007 <amaben202phantom@gmail.com>
2024-02-22 17:14:24 +0100

    create access token and refresh tokens

--- src/controllers/auth/auth.controller.js
+++ src/controllers/auth/auth.controller.js
@@ -1,5 +1,17 @@
 export const register = async (req, res, next) => {
   try {
+    const newUser = {};
+    const access_token = genToken({
+      userId: newUser._id,
+      expiry: "1d",
+      secret: process.env.ACCESS_TOKEN_SECRET,
+    });
+
+    const refresh_token = genToken({
+      userId: newUser._id,
+      expiry: "30d",
+      secret: process.env.REFRESH_TOKEN_SECRET,
+    });
   } catch (error) {
     next(error);
   }
