cb52834d8e48cd997da4c5c83b1f0c84307fae78
algomachine007 <amaben202phantom@gmail.com>
2024-02-22 18:11:26 +0100

    user create functionality

--- src/controllers/auth/auth.controller.js
+++ src/controllers/auth/auth.controller.js
@@ -1,9 +1,19 @@
+import { createUser } from "../../utils/createUser";
+import { generateToken } from "../../utils/generateToken";
+
 const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = process.env;
 
 export const register = async (req, res, next) => {
   try {
-    const newUser = {};
-    const access_token = generateToken(
+    const { name, email, picture, password, status } = req.body;
+    const newUser = await createUser({
+      name,
+      email,
+      picture,
+      password,
+      status,
+    });
+    const access_token = await generateToken(
       {
         userId: newUser._id,
       },
@@ -11,13 +21,31 @@ export const register = async (req, res, next) => {
       "1d",
     );
 
-    const refresh_token = genToken(
+    const refresh_token = await generateToken(
       {
         userId: newUser._id,
       },
       REFRESH_TOKEN_SECRET,
       "30d",
     );
+
+    // attach the refresh token to cookie
+    res.cookie("refreshToken", refresh_token, {
+      httpOnly: true,
+      path: "/api/v1/auth/refreshToken",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    console.table(refresh_token, access_token);
+    // send the user with access_token and data
+
+    res.status(201).json({
+      message: "Registration successful",
+      access_token,
+      user: {
+        ...newUser,
+      },
+    });
   } catch (error) {
     next(error);
   }
