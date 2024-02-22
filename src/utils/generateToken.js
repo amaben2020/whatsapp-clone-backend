ca4ff523dc09336f1501772bd9a63206665b579a
algomachine007 <amaben202phantom@gmail.com>
2024-02-22 17:24:44 +0100

    gen token func

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -2,5 +2,11 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = async (payload, secret, expiry) => {
   // payload: an object that represents the data to be sent to client as token
-  let token = await jwt.sign(payload, secret, { expiresIn: expiry });
+  try {
+    let token = await jwt.sign(payload, secret, { expiresIn: expiry });
+
+    return token;
+  } catch (error) {
+    throw createHttpError.BadRequest(error);
+  }
 };
