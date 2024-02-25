2d4afc4a9fafbd2b335e183a2086fbd7582b4a04
algomachine007 <amaben202phantom@gmail.com>
2024-02-25 08:10:59 +0100

    routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import authRoutes from "./auth.routes.js";
+import conversationRoutes from "./conversation.routes.js";
 const router = express.Router();
 
 // all resources and endpoints here
 router.use("/auth", authRoutes);
+router.use("/conversations", conversationRoutes);
 
 export default router;
