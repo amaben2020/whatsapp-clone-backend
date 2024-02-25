643669a6f5a2641e04957451fe029e115890fb8d
algomachine007 <amaben202phantom@gmail.com>
2024-02-25 08:14:37 +0100

    routing

--- src/routes/conversation.routes.js
+++ src/routes/conversation.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import trimRequest from "trim-request";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.route("/").post(trimRequest.all, () => {});
+router.route("/").post(trimRequest.all, authMiddleware, () => {});
 
 export default router;
