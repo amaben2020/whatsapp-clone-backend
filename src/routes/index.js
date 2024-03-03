ac8860d0a6dbbf25f9d3951b4d327f61eb0698c4
algomachine007 <benneth@izesan.com>
2024-03-03 12:57:28 +0100

    creating messages routes and controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authRoutes from "./auth.routes.js";
 import conversationRoutes from "./conversation.routes.js";
+import messageRoutes from "./message.routes.js";
 const router = express.Router();
 
 // all resources and endpoints here
 router.use("/auth", authRoutes);
 router.use("/conversations", conversationRoutes);
+router.use("/messages", messageRoutes);
 
 export default router;
