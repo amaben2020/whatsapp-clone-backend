039f1e3d97f3209418312722c20611065c9bbbaa
algomachine007 <benneth@izesan.com>
2024-03-03 12:53:21 +0100

    creating mongoose message model and schema

--- src/models/MessageModel.js
+++ src/models/MessageModel.js
@@ -2,9 +2,30 @@ import mongoose from "mongoose";
 
 const { ObjectId } = mongoose.Schema;
 
-const MessageModel = mongoose.Schema({
-  sender: {
-    type: ObjectId,
-    ref: "UserModel",
+const MessageSchema = mongoose.Schema(
+  {
+    sender: {
+      type: ObjectId,
+      ref: "UserModel",
+    },
+    message: {
+      type: String,
+      trim: true,
+    },
+    conversation: {
+      type: ObjectId,
+      ref: "ConversationModel",
+    },
+    files: [],
   },
-});
+  {
+    collection: "messages",
+    timestamps: true,
+  },
+);
+
+// if a message model exists, use it, otherwise, create new one
+const MessageModel =
+  mongoose.models.MessageModel || mongoose.model("MessageModel", MessageSchema);
+
+export default MessageModel;
