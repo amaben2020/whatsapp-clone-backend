50c704138dcee202b49657a07222637a4a731b41
algomachine007 <amaben202phantom@gmail.com>
2024-03-10 16:18:06 +0100

    adding user routing mechanism

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import authRoutes from "./auth.routes.js";
 import conversationRoutes from "./conversation.routes.js";
 import messageRoutes from "./message.routes.js";
+import userRoutes from "./user.routes.js";
 const router = express.Router();
 
 // all resources and endpoints here
 router.use("/auth", authRoutes);
 router.use("/conversations", conversationRoutes);
 router.use("/messages", messageRoutes);
+router.use("/user", userRoutes);
 
 export default router;
